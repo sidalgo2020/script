dc34ccc1f8f8b421224f7dbbc4043595e385930f
sidalgo2020 <69898876+sidalgo2020@users.noreply.github.com>
2020-08-19 17:06:25 +0700

    Add files via upload

---
+++ pixelfb.js
@@ -0,0 +1,16 @@
+<script>
+ !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version=&#39;2.0&#39;;
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,&#39;script&#39;,
+  &#39;https://connect.facebook.net/en_US/fbevents.js&#39;);
+fbq(&#39;init&#39;, &#39;&#39;);
+
+fbq(&#39;init&#39;, &#39;615800536018747&#39;);
+fbq(&#39;init&#39;, &#39;2943545902379717&#39;);
+  fbq(&#39;track&#39;, &#39;PageView&#39;);
+fbq(&#39;track&#39;, &#39;ViewContent&#39;);
+</script>
